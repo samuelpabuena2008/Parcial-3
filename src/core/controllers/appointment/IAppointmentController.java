768cc260c56d25c8874165ca236ed9640a4f9868
is0gvs <is0gvs@github.com>
2026-05-23 18:07:34 -0500

    refactor: limpiar codigo muerto autogenerado y arreglar nombres de variables

---
+++ src/core/controllers/appointment/IAppointmentController.java
@@ -0,0 +1,19 @@
+package core.controllers.appointment;
+
+import core.controllers.utils.Response;
+import core.controllers.utils.Status;
+
+public interface IAppointmentController {
+    Response requestAppointmentByDoctor(long patientId, long doctorId, String dateStr, String timeStr, String reason, boolean inPerson);
+    Response requestAppointmentBySpecialty(long patientId, String specialtyName, String dateStr, String timeStr, String reason, boolean inPerson);
+    Response cancelAppointment(String appointmentId);
+    Response getPatientAppointments(long patientId);
+    
+    Response acceptAppointment(String appointmentId);
+    Response completeAppointment(String appointmentId, String diagnosis, String observations, String recommendedTreatment, String followUp);
+    Response rescheduleAppointment(String appointmentId, String newTimeStr, String reason);
+    Response prescribeMedication(String appointmentId, String medicationName, String doseStr, String administrationRoute, String treatmentDurationStr, String additionalInstructions, String frequencyStr);
+    Response getDoctorAppointments(long doctorId, boolean pendingOnly);
+}
+
+
