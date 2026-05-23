2e9f93d74a7a9179c0db4b843cf9b91775b61075
Karel2710 <karels@uninorte.edu.co>
2026-05-23 15:50:27 -0500

    refactor: pasar clases de Administrator y Doctor al modelo MVC

---
+++ src/core/models/Administrator.java
@@ -0,0 +1,29 @@
+package core.models;
+
+import core.models.*;
+import core.models.enums.*;
+import core.controllers.utils.Response;
+
+import java.util.Map;
+
+/**
+ * Modelo de administrador del sistema.
+ * El administrador puede realizar las mismas acciones que un paciente y un doctor.
+ * 
+ * @author edangulo
+ */
+public class Administrator extends User {
+
+    public Administrator(long id, String username, String firstname, String lastname, String password) {
+        super(id, username, firstname, lastname, password);
+    }
+
+    @Override
+    public Map<String, String> serialize() {
+        Map<String, String> data = super.serialize();
+        data.put("type", "admin");
+        return data;
+    }
+}
+
+
---
+++ src/core/models/Doctor.java
@@ -0,0 +1,89 @@
+package core.models;
+
+import core.models.*;
+import core.models.enums.*;
+import core.controllers.utils.Response;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Modelo de doctor. Hereda de User y agrega especialidad, licencia y oficina.
+ * Contiene las listas de citas y hospitalizaciones asignadas.
+ * 
+ * @author edangulo
+ */
+public class Doctor extends User {
+
+    private Specialty specialty;
+    private String licenceNumber;
+    private String assignedOffice;
+    private ArrayList<Appointment> appointments;
+    private ArrayList<Hospitalization> hospitalizations;
+
+    public Doctor(long id, String username, String firstname, String lastname, String password,
+                  Specialty specialty, String licenceNumber, String assignedOffice) {
+        super(id, username, firstname, lastname, password);
+        this.specialty = specialty;
+        this.licenceNumber = licenceNumber;
+        this.assignedOffice = assignedOffice;
+        this.appointments = new ArrayList<>();
+        this.hospitalizations = new ArrayList<>();
+    }
+
+    // --- Getters ---
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
+
+    public String getAssignedOffice() {
+        return assignedOffice;
+    }
+
+    public ArrayList<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public ArrayList<Hospitalization> getHospitalizations() {
+        return hospitalizations;
+    }
+
+    // --- Setters ---
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
+
+    public void setLicenceNumber(String licenceNumber) {
+        this.licenceNumber = licenceNumber;
+    }
+
+    public void setAssignedOffice(String assignedOffice) {
+        this.assignedOffice = assignedOffice;
+    }
+
+    public void addAppointment(Appointment a) {
+        this.appointments.add(a);
+    }
+
+    public boolean addHospitalization(Hospitalization hosp) {
+        return hospitalizations.add(hosp);
+    }
+
+    @Override
+    public Map<String, String> serialize() {
+        Map<String, String> data = super.serialize();
+        data.put("type", "doctor");
+        data.put("specialty", specialty.name());
+        data.put("licenceNumber", licenceNumber);
+        data.put("assignedOffice", assignedOffice);
+        return data;
+    }
+}
+
+
