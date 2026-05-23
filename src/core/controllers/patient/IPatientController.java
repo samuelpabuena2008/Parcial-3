6f7122b8073244d4cf99ced285e19280654bbe80
Samuel Pabuena <samuelpabuena@example.com>
2026-05-23 14:35:00 -0500

    refactor: pasar metodos de los pacientes a su propio controlador

--- src/core/controllers/patient/IPatientController.java
+++ src/core/controllers/patient/IPatientController.java
@@ -1,3 +1,15 @@
 package core.controllers.patient;
 
-public interface IPatientController {}
+import core.controllers.utils.Response;
+import core.controllers.utils.Status;
+
+public interface IPatientController {
+    Response registerPatient(String idStr, String username, String firstname, String lastname, String password, String confirmPassword, String phone, String email, boolean gender, String address, String birthdateStr);
+    Response updatePatient(long currentId, String username, String firstname, String lastname, String password, String confirmPassword, String phone, String email, boolean gender, String address, String birthdateStr);
+    Response getPatientData(long patientId);
+    Response getDoctorList();
+    Response getDoctorsBySpecialty(String specialtyName);
+    Response getPatientHospitalizations(long patientId);
+}
+
+
