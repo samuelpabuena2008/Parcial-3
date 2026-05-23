62c65af21d79b88011ec8ab68f051e076be3a112
Samuel Pabuena <samuelpabuena@example.com>
2026-05-23 12:05:00 -0500

    feat: agregar clases vacias para empezar login y pacientes

---
+++ src/core/controllers/login/LoginController.java
@@ -0,0 +1,3 @@
+package core.controllers.login;
+
+public class LoginController implements ILoginController {}
---
+++ src/core/controllers/patient/PatientController.java
@@ -0,0 +1,3 @@
+package core.controllers.patient;
+
+public class PatientController implements IPatientController {}
